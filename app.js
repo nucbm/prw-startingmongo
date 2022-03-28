8fac849d7bb34f8b186f3c80d77c5c822f2f67f3
calculatoare <costea.c@gmail.com>
2022-03-29 00:56:45 +0300

    update: db.close

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(LoggerMiddleware);
 
 
+let uri = "mongodb://localhost:27017/laboratoare";
+let options = { useUnifiedTopology: true, useNewUrlParser: true };
 
 // Function to handle the root path
 app.get('/', async function(req, res) {
@@ -30,54 +32,59 @@ app.get('/', async function(req, res) {
     <ul>
         <li><a href='/connection'>Connection</a>
         <li><a href='/collections'>List Collections </a>
-        <li><a href='/list'>List items</a>
-        <li><a href='/insert'>Insert item</a> 
+        <li><a href='/list'>List items: Curriculum</a>
+        <li><a href='/insert'>Insert item: OneBook</a> 
+        <li><a href='/onebook'>List items: OneBook</a>
         <li><a href='/drop'>Drop collection</a>
-        <li><a href='/update'>Update item</a><br/>
     </ul>
 </html>`);
 });
 
+//         <li><a href='/update'>Update item</a><br/>
 
 app.get('/connection', (req,res) => {
-    var uri = "mongodb://localhost:27017/laboratoare";
-    mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
-    const connection = mongoose.connection;
+  mongoose.connect(uri, options);
+  let db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
 
-    connection.once("open", function() {
+  db.once("open", function(err,resp) {
       console.log("MongoDB database connection established successfully");
-    });
+  });
 
-    res.json({
-      'status':true
-    })
+  res.json({
+    'status':true
+  });
+  db.close();
 })
 
 
 
 app.get('/collections', (req,res) => {
-  var uri = "mongodb://localhost:27017/laboratoare";
-  mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
-  
-  mongoose.connection.on('open', function (ref) {
+  //mongoose.connect(uri, options);
+  //let dbc = mongoose.connection;
+  var dbc = mongoose.createConnection(uri, options);
+  let strResponse="<html><body><ul>";
+
+  dbc.once('open', function (ref) {
     console.log('Connected to mongo server.');
-    //trying to get collection names
-    mongoose.connection.db.listCollections().toArray(function (err, names) {
-        console.log(names); // [{ name: 'dbname.myCollection' }]
-    });
-  })
-  res.json({
-    'status':true
+    dbc.db.listCollections().toArray(function (err, names) {
+        //console.log(names); // [{ name: 'dbname.myCollection' }]
+        console.log(names.length + " inregistrari");
+        names.forEach(function (item, key) {
+          strResponse += '<li>' + item.name +'</li>';
+          console.log(' - ' + item.name);
+          //strResponse = strResponse + '<li>' + item.name +'</li>';
+        })
+        strResponse += "</ul></body></html>";
+        res.send(strResponse);
+    })
   })
 })
 
 
+
 app.get('/list',(req,res)=>{
-  var uri = "mongodb://localhost:27017/laboratoare";
-  mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
-  
-  // get reference to database
-  var db = mongoose.connection;
+  var db = mongoose.createConnection(uri, options);
   db.on('error', console.error.bind(console, 'connection error:'));
  
   db.once('open', function() {
@@ -97,26 +104,19 @@ app.get('/list',(req,res)=>{
 })
   
 
-var last_id;
 
 app.get('/insert',(req,res)=>{
-  var uri = "mongodb://localhost:27017/laboratoare";
-  mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
-  
-  // get reference to database
-  var db = mongoose.connection;
+  var db = mongoose.createConnection(uri, options);
   db.on('error', console.error.bind(console, 'connection error:'));
  
   db.once('open', function() {
     //console.log("Connection Successful!");
-    const booksCollection = db.collection("books");
+    const booksCollection = db.collection("onebook");
     var newItem = {authors:"Amit Phaltankar, Juned Ahsan, Michael Harrison, Liviu Nedov", title:"MongoDB Fundamentals: A hands-on guide to using MongoDB and Atlas in the real world"}
 
     booksCollection.insertOne(newItem)
       .then(result => {
-        last_id = result.insertedId;
         console.log(`Successfully inserted item with _id: ${result.insertedId}`)
-        //console.log('am actualizat last_id:' + last_id);
       })  
       .catch(err => console.error(`Failed to insert item: ${err}`))
 
@@ -128,6 +128,31 @@ app.get('/insert',(req,res)=>{
 
 
 
+
+app.get('/onebook',(req,res)=>{
+  var db = mongoose.createConnection(uri, options);
+  db.on('error', console.error.bind(console, 'connection error:'));
+ 
+  db.once('open', function() {
+    //console.log("Connection Successful!");
+
+    const collection = db.collection("onebook");
+    console.log('\nOneBook collection:');
+    collection.find().toArray((err, items) => {
+      items.forEach(function (item,key) {
+          console.log(' - ' + item.title);
+      })
+    });
+  })
+  res.json({
+    'status':true
+  })
+})
+  
+
+
+
+
 app.get('/drop', (req,res) => {
   var uri = "mongodb://localhost:27017/laboratoare";
   mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -136,10 +161,9 @@ app.get('/drop', (req,res) => {
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
 
-
   db.once('open', function() {
-    db.dropCollection("books", function(err, result) {
-      console.log("Collection droped");
+    db.dropCollection("onebook", function(err, result) {
+      console.log("Collection 'onebook' droped");
     });
   })
   res.json({'status':true})
